d56ded032309349bad949864adfb03f62525649c
DitoGeorgia <ditogeorgia@gmail.com>
2025-02-01 05:51:37 +0400

    add

---
+++ Lessons/src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+        int a = 100;
+        String string = "Hello";
+        String space = " ";
+        String name = "Bob";
+
+        System.out.println(string+space+name);
+        System.out.println("Hello" + " " + "John");
+        System.out.println("My number is " + a);
+    }
+}
---
+++ Lessons/src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt;
+        myInt = 15;
+        System.out.println(myInt);
+
+        int myInt2 = 20;              // 32 бита
+        System.out.println(myInt2);
+
+        short myShort = 30;  // 16 бит
+        long myLong = 1000;  // 64 бита
+
+        double myDouble = 235.50; // 64
+        float myFloat = 159.56f;  // 32
+
+        char c = 'c';
+        boolean b = true;    //  false
+        boolean b2 = false;  //  true
+
+        byte myByte = 8;     // 8 бит
+
+    }
+}
