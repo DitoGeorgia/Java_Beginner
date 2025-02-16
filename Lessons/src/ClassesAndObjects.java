28a9981d55a7c93349231fa51b606b570ae90d52
DitoGeorgia <ditogeorgia@gmail.com>
2025-02-16 05:16:23 +0400

    add

--- Lessons/src/ClassesAndObjects.java
+++ Lessons/src/ClassesAndObjects.java
@@ -1,30 +1,13 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
-//        Person person1 = new Person();
-//        person1.name = "Роман";
-//        person1.age = 50;
-//        System.out.println("Меня зовут " + person1.name + ", мне " + person1.age + " лет.");
-//
-//        Person person2 = new Person();
-//        person2.name = "Вова";
-//        person2.age = 20;
-//        System.out.println("Меня зовут " + person2.name + ", мне " + person2.age + " лет.");
-
         Person person1 = new Person();
-        person1.name = "Роман";
-        person1.age = 50;
-        person1.speak();
-        person1.sayHello();
-        int year1 = person1.calculateYearsToRetirement();
-        System.out.println("До пенсии: " + year1 + " лет.\n");
+        person1.setName("");
+        person1.setAge(0);
 
-        Person person2 = new Person();
-        person2.name = "Вова";
-        person2.age = 20;
-        person2.speak();
-        person2.sayHello();
-        int year2 = person2.calculateYearsToRetirement();
-        System.out.println("До пенсии: " + year2 + " лет.");
+        System.out.println("Выводим значение в main методе: " + person1.getName());
+
+        System.out.println("Выводим значение в main методе: " + person1.getAge());
+
     }
 }
 
@@ -34,8 +17,34 @@ public class ClassesAndObjects {
 // 2. Действия , которые он может совершать (методы)
 
 class Person {
-    String name;
-    int age;
+    private String name;
+    private int age;
+
+    public void setName(String userName) {
+        if (userName.isEmpty()) {
+            System.out.println("Ввод пустого имени!!!");
+        } else {
+            name = userName;
+        }
+    }
+    public String getName() {
+        return name;
+    }
+
+
+    public void setAge(int userAge) {
+        if (userAge <= 0) {
+            System.out.println("Возраст не верный");
+        } else {
+            age = userAge;
+        }
+    }
+    public int getAge() {
+        return age;
+    }
+
+
+
 
     void speak() {
         System.out.println("Меня зовут " + name + ", мне " + age + " лет.");
---
+++ Lessons/src/Lesson20.java
@@ -0,0 +1,38 @@
+public class Lesson20 {
+    public static void main(String[] args) {
+        Human h1 = new Human("Bob",15);
+        Human h2 = new Human("Tom",25);
+        h1.printNumberOfPeople();
+        h2.printNumberOfPeople();
+
+
+
+    }
+}
+
+class Human {
+    private String name;
+    private int age;
+
+    private static int countPeople;
+
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+        countPeople++;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void printNumberOfPeople() {
+        System.out.println("Number of people is " + countPeople);
+    }
+}
